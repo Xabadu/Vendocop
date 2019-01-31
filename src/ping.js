d1ea008ff4fe3e04d467a505627f33a523131d6a
Fernando Larrañaga <xabadu@gmail.com>
2019-01-31 11:16:38 -0800

    Remove vendor filter (#41)

--- src/ping.js
+++ src/ping.js
@@ -4,15 +4,10 @@ var orm = require('../lib/orm');
 var parser = require('../lib/parser');
 
 var pings = {
-  check: async (function(vendor) {
+  check: async (function() {
     var maxTimeDiff, stores;
-    if(typeof vendor !== 'undefined') {
-      maxTimeDiff = 1500;
-      stores = await (orm.getStores('exclude', [1, 5, 6, 11, 901, 902, 903], vendor));
-    } else {
-      maxTimeDiff = 16200;
-      stores = await (orm.getStores('exclude', [1, 5, 6, 11, 901, 902, 903]));
-    }
+    maxTimeDiff = 16200;
+    stores = await(orm.getStores('exclude', [1, 5, 6, 11, 901, 902, 903]));
     stores.forEach(function(store) {
       var ping = await (orm.getLastPing(store.store_id));
       console.log('Last ping for store: ' + store);
@@ -40,4 +35,4 @@ var pings = {
   })
 };
 
-pings.check(process.argv[2]);
+pings.check();
